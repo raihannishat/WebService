7e64a286437e60b210e1dbda1ced977ba3f032ff
raihannishat <raihannishat.swe@gmail.com>
2022-10-18 14:29:08 +0600

    CRUD with a .NET 6 Web API & Entity Framework Core 🚀 Full Course

---
+++ SuperHero/SuperHero.Api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHero.Api.Data;
+
+public class DataContext : DbContext
+{
+	public DataContext(DbContextOptions<DataContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<SuperHero>? SuperHeroes { get; set; }
+}
