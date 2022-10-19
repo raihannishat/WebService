e325e589b5bfebd12e94d8a6f5be8df7f1a69b58
raihannishat <raihannishat.swe@gmail.com>
2022-10-19 14:49:29 +0600

    .NET 6 Web API 🔒 Read JWT Authorization Claims of a User (from a JSON Web Token)

--- 04_JwtWebTokenClaimsBasedAuthorization/JwtWebToken.Api/Program.cs
+++ 04_JwtWebTokenClaimsBasedAuthorization/JwtWebToken.Api/Program.cs
@@ -6,7 +6,6 @@ using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
