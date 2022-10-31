02971703e89d6c4c3e929924a478c1134aa75edc
raihannishat <raihannishat.swe@gmail.com>
2022-10-31 22:43:23 +0600

    Angular 14 & .NET 6 🔒 Authorization with JSON Web Tokens (JWT)

---
+++ 06_Angular14&.NET6/JwtWebToken.Api/Controllers/AuthController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JwtWebToken.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    public static User user = new();
+
+    private readonly IConfiguration _configuration;
+    private readonly IUserService _userService;
+
+    public AuthController(IConfiguration configuration, IUserService userService)
+    {
+        _configuration = configuration;
+        _userService = userService;
+    }
+
+    [HttpGet("getme"), Authorize]
+    public ActionResult<string> GetMe()
+    {
+        var userName = _userService.GetMyName();
+
+        return Ok(userName);
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(UserDto request)
+    {
+        CreatePasswordHash(request.password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        user.UserName = request.username;
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(UserDto request)
+    {
+        if (user.UserName != request.username)
+        {
+            return BadRequest("User not found");
+        }
+
+        if (!VerifyPasswordHash(request.password, user.PasswordHash!, user.PasswordSalt!))
+        {
+            return BadRequest("Wrong password");
+        }
+
+        var token = CreateToken(user);
+
+        var refreshToken = GetRefreshToken();
+        SetRefreshToken(refreshToken);
+
+        return Ok(token);
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<ActionResult<string>> RefreshToken()
+    {
+        var refreshToken = Request.Cookies["refreshToken"];
+
+        if (!user.RefreshToken.Equals(refreshToken))
+        {
+            return Unauthorized("Incalid refresh token.");
+        }
+        else if (user.TokenExpires < DateTime.Now)
+        {
+            return Unauthorized("Token expired.");
+        }
+
+        var token = CreateToken(user);
+        var newRefreshToken = GetRefreshToken();
+        SetRefreshToken(newRefreshToken);
+
+        return Ok(token);
+    }
+
+    private RefreshToken GetRefreshToken()
+    {
+        var refreshToken = new RefreshToken
+        {
+            Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+            Expires = DateTime.Now.AddDays(7),
+            Created = DateTime.Now
+        };
+
+        return refreshToken;
+    }
+
+    private void SetRefreshToken(RefreshToken refreshToken)
+    {
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            Expires = refreshToken.Expires,
+        };
+
+        Response.Cookies.Append("refreshToken", refreshToken.Token, cookieOptions);
+
+        user.RefreshToken = refreshToken.Token;
+        user.TokenCreated = refreshToken.Created;
+        user.TokenExpires = refreshToken.Expires;
+    }
+
+    private string CreateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Role, "Admin")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            _configuration.GetSection("AppSettings:Token").Value));
+
+        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: cred);
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+    }
+
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512(user.PasswordSalt!))
+        {
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return computeHash.SequenceEqual(passwordHash);
+        }
+    }
+}
---
+++ 06_Angular14&.NET6/JwtWebToken.Api/UserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtWebToken.Api;
+
+public class UserDto
+{
+    public string username { get; set; } = string.Empty;
+    public string password { get; set; } = string.Empty;
+}
